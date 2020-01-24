c0eb905d83dd27605d0d15286e8b8fd6e620d78d
guidezpl <guidezpl@users.noreply.github.com>
2020-01-24 15:00:12 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ DATA_DIR=data
 LANGUAGES_INDEX="$PWD/languages.json"
 
 [[ ! -d "$DATA_DIR" ]] || rm -R "$DATA_DIR"
-curl -L https://github.com/umpirsky/country-list/archive/master.tar.gz | tar -xf - '*/country.json'
-find . -name 'country.json' -exec bash -c 'mv $1 $(echo $1 | sed -e "s/\/country\.json/\.json/g")' -- {} \;
-ls -A -d country-list-master/data/*/ | xargs rmdir
-mv country-list-master/data "$DATA_DIR"
-rmdir country-list-master
+curl -L https://github.com/umpirsky/locale-list/archive/master.tar.gz | tar -xf - '*/locales.json'
+find . -name 'locales.json' -exec bash -c 'mv $1 $(echo $1 | sed -e "s/\/locales\.json/\.json/g")' -- {} \;
+ls -A -d locale-list-master/data/*/ | xargs rmdir
+mv locale-list-master/data "$DATA_DIR"
+rmdir locale-list-master
 
 cd "$DATA_DIR"
 find . -name '*.json' | sed -E "s/\.\///;s/\.json//" | jq -nRc '[inputs | select(length>0)] | sort' > "$LANGUAGES_INDEX"
