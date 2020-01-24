1fc95631950e7fb27ad5395dfa4965efaf0ba4e5
guidezpl <guidezpl@users.noreply.github.com>
2020-01-24 15:00:02 +0100

    Remove country tests and add locale tests

--- test/flutter_localized_countries_test.dart
+++ test/flutter_localized_countries_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_localized_countries/flutter_localized_countries.dart';
+import 'package:flutter_localized_locales/flutter_localized_locales.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
@@ -9,9 +9,10 @@ import 'dart:typed_data';
 class TestAssetBundle extends CachingAssetBundle {
   @override
   Future<ByteData> load(String key) async {
-    const prefix = "packages/flutter_localized_countries/";
+    const prefix = "packages/flutter_localized_locales/";
     if (key.startsWith(prefix)) {
-      var path = join(dirname(Platform.script.toFilePath()), key.substring(prefix.length));
+      var path = join(
+          dirname(Platform.script.toFilePath()), key.substring(prefix.length));
       var bytes = Uint8List.fromList(await File(path).readAsBytes());
       var buffer = bytes.buffer;
       return ByteData.view(buffer);
@@ -22,45 +23,65 @@ class TestAssetBundle extends CachingAssetBundle {
 
 void main() {
   final bundle = TestAssetBundle();
-  var delegate = CountryNamesLocalizationsDelegate(bundle: bundle);
+  var delegate = LocaleNamesLocalizationsDelegate(bundle: bundle);
   test('provides list of locales()', () {
     expect(delegate.locales(), completion(isNotEmpty));
   });
 
-  void checkTranslation(Locale locale, String cc, String name) {
-    var d = delegate;
-    var f = (CountryNames cn) => cn.nameOf(cc) == name;
+  var localeDelegate = LocaleNamesLocalizationsDelegate(bundle: bundle);
+  test('Locale delegate provides list of locale codes', () {
+    expect(localeDelegate.locales(), completion(isNotEmpty));
+  });
+
+  test('Locale delegate provides map of native locale names', () {
+    expect(localeDelegate.allNativeNames(), completion(isNotEmpty));
+  });
+
+  void checkLocaleTranslation(Locale locale, String cc, String name) {
+    var d = localeDelegate;
+    var f = (LocaleNames cn) => cn.nameOf(cc) == name;
     var matcher = completion(predicate(f, 'name of the $cc is "$name"'));
     expect(d.load(locale), matcher);
   }
-  
-  test('localizes by language', () {
-    checkTranslation(Locale('de'), 'CH', 'Schweiz');
-    checkTranslation(Locale('en'), 'CH', 'Switzerland');
-    checkTranslation(Locale('ja'), 'CH', 'スイス');
-    checkTranslation(Locale('de'), 'CH', 'Schweiz');
-  });
 
-  test('localizes by language and country', () {
-    checkTranslation(Locale('de', 'CH'), 'BY', 'Weissrussland');
-    checkTranslation(Locale('de', 'AT'), 'BY', 'Belarus');
-    checkTranslation(Locale('de', 'CH'), 'GB', 'Grossbritannien');
-    checkTranslation(Locale('de'), 'GB', 'Vereinigtes Königreich');
+  test('localizes locale by language', () {
+    checkLocaleTranslation(Locale('de'), 'de_CH', 'Deutsch (Schweiz)');
+    checkLocaleTranslation(Locale('en'), 'de_CH', 'German (Switzerland)');
+    checkLocaleTranslation(Locale('ja'), 'de_CH', 'ドイツ語 (スイス)');
+    checkLocaleTranslation(Locale('de'), 'de_CH', 'Deutsch (Schweiz)');
   });
-  test('language and country falls back to language only', () {
-    checkTranslation(Locale('de', 'UK'), 'GB', 'Vereinigtes Königreich');
+  test('localizes locale by language and country', () {
+    checkLocaleTranslation(Locale('de', 'CH'), 'be', 'Weissrussisch');
+    checkLocaleTranslation(Locale('de', 'AT'), 'be', 'Weißrussisch');
+    checkLocaleTranslation(
+        Locale('de', 'CH'), 'en_GB', 'Englisch (Grossbritannien)');
+    checkLocaleTranslation(
+        Locale('de'), 'en_GB', 'Englisch (Vereinigtes Königreich)');
+    checkLocaleTranslation(
+        Locale('de', 'CH'), 'en_GB', 'Englisch (Grossbritannien)');
+    checkLocaleTranslation(
+        Locale('de'), 'en_GB', 'Englisch (Vereinigtes Königreich)');
   });
-  test('language falls back to English', () {
-    checkTranslation(Locale('zz'), 'GB', 'United Kingdom');
+  test('invalid locale gives null', () {
+    checkLocaleTranslation(Locale('de'), 'zz', null);
   });
-  test('languages can be sorted by code and name', () {
+  test(
+      'localized locale falls back to language when given invalid country for locale',
+      () {
+    checkLocaleTranslation(
+        Locale('de', 'UK'), 'es_AR', 'Spanisch (Argentinien)');
+  });
+  test('localized locale falls back to English when given invalid locale', () {
+    checkLocaleTranslation(Locale('zz'), 'es_AR', 'Spanish (Argentina)');
+  });
+  test('locale names can be sorted by code and name', () {
     // TODO: Sorting by name should be done in locale-aware manner
-    final cn = CountryNames('foo', {
-      "BL": "St. Barthélemy",
-      "DE": "Germany",
-      "US": "United States",
+    final cn = LocaleNames('foo', {
+      "de": "German",
+      "ur_IN": "Urdu (India)",
+      "bo": "Tibetan",
     });
-    expect(cn.sortedByCode.map((e) => e.key), ['BL', 'DE', 'US']);
-    expect(cn.sortedByName.map((e) => e.key), ['DE', 'BL', 'US']);
+    expect(cn.sortedByCode.map((e) => e.key), ['bo', 'de', 'ur_IN']);
+    expect(cn.sortedByName.map((e) => e.key), ['de', 'bo', 'ur_IN']);
   });
 }
