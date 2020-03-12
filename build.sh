f4022dcb54450dc4c09f97cd70c82274a59a8155
guidezpl <guidezpl@users.noreply.github.com>
2020-03-12 19:44:27 +0100

    Move locale_native_names.json and rename languages.json

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 cd $(dirname $0)
 
 DATA_DIR=data
-LANGUAGES_INDEX="$PWD/languages.json"
+LOCALES_INDEX="$PWD/locales.json"
 
 [[ ! -d "$DATA_DIR" ]] || rm -R "$DATA_DIR"
 curl -L https://github.com/umpirsky/locale-list/archive/master.tar.gz | tar -xf - '*/locales.json'
@@ -14,4 +14,4 @@ mv locale-list-master/data "$DATA_DIR"
 rmdir locale-list-master
 
 cd "$DATA_DIR"
-find . -name '*.json' | sed -E "s/\.\///;s/\.json//" | jq -nRc '[inputs | select(length>0)] | sort' > "$LANGUAGES_INDEX"
+find . -name '*.json' | sed -E "s/\.\///;s/\.json//" | jq -nRc '[inputs | select(length>0)] | sort' > "$LOCALES_INDEX"
