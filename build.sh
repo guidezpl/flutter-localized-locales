d51696db61d25f91f5ba7797d6ab111c43c8306e
guidezpl <guidezpl@users.noreply.github.com>
2020-03-12 19:55:43 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ LOCALES_INDEX="$PWD/locales.json"
 
 [[ ! -d "$DATA_DIR" ]] || rm -R "$DATA_DIR"
 curl -L https://github.com/umpirsky/locale-list/archive/master.tar.gz | tar -xf - '*/locales.json'
-find . -name 'locales.json' -exec bash -c 'mv $1 $(echo $1 | sed -e "s/\/locales\.json/\.json/g")' -- {} \;
+find locale-list-master/data -name 'locales.json' -exec bash -c 'mv $1 $(echo $1 | sed -e "s/\/locales\.json/\.json/g")' -- {} \;
 ls -A -d locale-list-master/data/*/ | xargs rmdir
 mv locale-list-master/data "$DATA_DIR"
 rmdir locale-list-master
